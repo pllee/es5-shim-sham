b3135d891aa9be695d8c04edbb253349da6d6fc9
pllee <patrick.lorian.lee@gmail.com>
2013-12-12 19:47:50 -0600

    add es5 license header

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+/**
+ * @license https://raw.github.com/kriskowal/es5-shim/master/LICENSE
+ * es5-shim license
+ */
 require('./node_modules/es5-shim/es5-shim');
 require('./node_modules/es5-shim/es5-sham');
